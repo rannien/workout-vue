96e147ec359f47df5fad3008460475ca5831da30
Csaba Verebélyi <verebelyi.csaba@zengo.ez>
2022-07-30 19:06:56 +0200

    feat: add greeting

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { useAuthStore } from '../stores/AuthStore';
 
 const count = ref(0);
+
+const { user } = useAuthStore();
 </script>
 
 <template>
   <div>
-    <h1>Hello Vue 3 + TypeScript + Vite</h1>
-    <p>
-      Recommended IDE setup:
-      <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-      +
-      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    </p>
-
-    <p>See <code>README.md</code> for more information.</p>
-
-    <p>
-      <a href="https://vitejs.dev/guide/features.html" target="_blank">
-        Vite Docs
-      </a>
-      |
-      <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-    </p>
+    <h1 class="text-3xl">
+      Hello <strong>{{ user?.username }}!</strong>
+    </h1>
 
     <button type="button" @click="count++">count is: {{ count }}</button>
-    <p>
-      Edit
-      <code>views/HelloWorldView.vue</code> to test hot module replacement.
-    </p>
   </div>
 </template>
 
